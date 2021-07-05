d70c6e7491bf5d2a15723cbabe75dd07f24d9949
OPterminator <63536981+OPterminator@users.noreply.github.com>
2021-07-06 02:08:11 +0530

    Create Egg Dropping.cpp

---
+++ Egg Dropping.cpp
@@ -0,0 +1,13 @@
+https://www.interviewbit.com/problems/egg-drop-problem/
+int Solution::solve(int A, int B) {
+    vector<int>dp(A+1, 0);
+    int m;
+    for(m = 0; dp[A] < B; m++)
+    {
+        for(int k = A; k>0; k--)
+        {
+            dp[k] += dp[k-1] + 1;
+        }
+    }
+    return m;
+}
