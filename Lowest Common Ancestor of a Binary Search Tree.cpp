4e9cf6be8cd6153a993655877c528e2e85d01ba7
OPterminator <63536981+OPterminator@users.noreply.github.com>
2021-07-06 01:17:35 +0530

    Create Lowest Common Ancestor of a Binary Search Tree.cpp

---
+++ Lowest Common Ancestor of a Binary Search Tree.cpp
@@ -0,0 +1,52 @@
+https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+
+Well, remember to take advantage of the property of binary search trees, which is, node -> left -> val < node -> val < node -> right -> val. Moreover, both p and q will be the descendants of the root of the subtree that contains both of them. And the root with the largest depth is just the lowest common ancestor. This idea can be turned into the following simple recursive code.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if ((root -> val > p -> val) && (root -> val > q -> val)) {
+            return lowestCommonAncestor(root -> left, p, q);
+        }
+        if ((root -> val < p -> val) && (root -> val < q -> val)) {
+            return lowestCommonAncestor(root -> right, p, q);
+        }
+        return root;
+    }
+};
+Of course, we can also solve it iteratively.
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
+    }
+};
