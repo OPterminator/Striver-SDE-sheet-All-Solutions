5cc09001398f0ce16410d0b528342bd77994282a
OPterminator <63536981+OPterminator@users.noreply.github.com>
2021-07-06 00:39:30 +0530

    Create Inorder Traversal without recursion.cpp

---
+++ Inorder Traversal without recursion.cpp
@@ -0,0 +1,48 @@
+ 
+/* A binary tree Node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    Node (int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+ 
+/* Iterative function for inorder tree
+   traversal */
+void inOrder(struct Node *root)
+{
+    stack<Node *> s;
+    Node *curr = root;
+ 
+    while (curr != NULL || s.empty() == false)
+    {
+        /* Reach the left most Node of the
+           curr Node */
+        while (curr !=  NULL)
+        {
+            /* place pointer to a tree node on
+               the stack before traversing
+              the node's left subtree */
+            s.push(curr);
+            curr = curr->left;
+        }
+ 
+        /* Current must be NULL at this point */
+        curr = s.top();
+        s.pop();
+ 
+        cout << curr->data << " ";
+ 
+        /* we have visited the node and its
+           left subtree.  Now, it's right
+           subtree's turn */
+        curr = curr->right;
+ 
+    } /* end of while */
+}
