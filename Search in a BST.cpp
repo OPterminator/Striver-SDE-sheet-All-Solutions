00afb75eba69285539e6e77b34fb493b9ecdf9a3
OPterminator <63536981+OPterminator@users.noreply.github.com>
2021-07-06 01:14:10 +0530

    Create Search in a BST.cpp

---
+++ Search in a BST.cpp
@@ -0,0 +1,8 @@
+https://leetcode.com/problems/search-in-a-binary-search-tree/
+
+TreeNode* searchBST(TreeNode* root, int val) {
+    while (root != nullptr && root->val != val) {
+      root = (root->val > val) ? root->left : root->right;
+    }
+    return root;
+}
